bc68e4f7c14cf37ede0f53c195c9f611dd6b2c3c
Abhinav <allenrai1530@gmail.com>
2022-07-17 19:53:57 +0530

    Widgets

--- src/Components/Widgets.js
+++ src/Components/Widgets.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import '../Css/Widgets.css';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed
+} from 'react-twitter-embed';
+import { Search } from '@material-ui/icons';
 const Widgets = () => {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+     <div className="widgets__input">
+     <Search className="widgets__searchIcon" />
+      <input placeholder="Search Twitter" type="text" />
+     </div>
+      <div className="widgets__widgetContainer">
+      <h2>What's Happening</h2>
+      <TwitterTweetEmbed tweetId={'858551177860055040'} />
+      <TwitterTimelineEmbed
+      sourceType='profile'
+      screenName='LyghtJr'
+      options={{height: 400}}
+      />
+      <TwitterShareButton
+      url={"https://github.com/Lyghtjr"}
+      options={{text: "Twitter Share Button", via: "LyghtJr"}}
+      />
+      </div>
     </div>
+
   )
 }
 
